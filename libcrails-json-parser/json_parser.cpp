876141a009453e85976f39bd6e77c7a83f7349f7
plaristote <michael@unetresgrossebite.com>
2022-08-18 19:28:53 +0200

    update api for request parsers

--- libcrails-json-parser/json_parser.cpp
+++ libcrails-json-parser/json_parser.cpp
@@ -1,18 +1,19 @@
 #include <boost/property_tree/json_parser.hpp>
 #include "json_parser.hpp"
-#include <crails/params.hpp>
+#include <crails/context.hpp>
 
 using namespace std;
 using namespace Crails;
 using namespace boost::property_tree;
 
-void RequestJsonParser::operator()(Connection& connection, BuildingResponse& out, Params& params, function<void(RequestParser::Status)> callback) const
+void RequestJsonParser::operator()(Context& context, function<void(RequestParser::Status)> callback) const
 {
   static const regex is_json("application/json", regex_constants::extended | regex_constants::optimize);
+  const HttpRequest& request = context.connection->get_request();
 
-  if (params["method"].as<string>() != "GET" && content_type_matches(params, is_json))
+  if (request.method() != HttpVerb::get && content_type_matches(request, is_json))
   {
-    wait_for_body(connection, out, params, [callback]()
+    wait_for_body(context, [callback]()
     {
       callback(RequestParser::Stop);
     });
@@ -21,8 +22,8 @@ void RequestJsonParser::operator()(Connection& connection, BuildingResponse& out
     callback(RequestParser::Continue);
 }
 
-void RequestJsonParser::body_received(Connection&, BuildingResponse&, Params& params, const string& body) const
+void RequestJsonParser::body_received(Context& context, const string& body) const
 {
   if (body.size() > 0)
-    params.as_data().merge(DataTree().from_json(body));
+    context.params.as_data().merge(DataTree().from_json(body));
 }
